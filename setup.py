2c2ebe3711de6020643949ed946b419093275cc4
Eugene Prikazchikov <eprikazc@gmail.com>
2015-12-29 22:58:17 +0300

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(
     name = "nose-desktop-notifications",
     version = "0.1",
     packages=['nose_desktop_notifications'],
-    # install_requires=[
-    #     '',
-    # ],
     entry_points={
-        # 'nose.plugins.0.10': [
         'nose.plugins': [
             'nose_desktop_notifications = nose_desktop_notifications:DesktopNotificationPlugin',
         ]
